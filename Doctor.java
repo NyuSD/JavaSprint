bfc4e7be16b5197fa9e187515766c382a2bd203e
NyuSD <161166895+NyuSD@users.noreply.github.com>
2025-02-18 20:17:44 -0330

    Added doctor class

--- Doctor.java
+++ Doctor.java
@@ -1,3 +1,41 @@
-public class Doctor {
-    
+import java.util.ArrayList;
+import java.util.List;
+
+public class Doctor extends Person {
+    private String specialization;
+    private List<Patient> patients;
+
+    public Doctor(String id, String name, int age, String phoneNumber, String specialization) {
+        super(id, name, age, phoneNumber);
+        this.specialization = specialization;
+        this.patients = new ArrayList<>();
+    }
+
+    public String getSpecialization() {
+        return specialization;
+    }
+
+    public void setSpecialization(String specialization) {
+        this.specialization = specialization;
+    }
+
+    public List<Patient> getPatients() {
+        return patients;
+    }
+
+    public void addPatient(Patient patient) {
+        this.patients.add(patient);
+    }
+
+    @Override
+    public String toString() {
+        return "Doctor{" +
+                "ID='" + getId() + '\'' +
+                ", name='" + getName() + '\'' +
+                ", age=" + getAge() +
+                ", phoneNumber='" + getPhoneNumber() + '\'' +
+                ", specialization='" + specialization + '\'' +
+                ", patients=" + patients +
+                '}';
+    }
 }
