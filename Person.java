3c97ca31104f1f06cda70db1b3bcee30c5018e2d
NyuSD <161166895+NyuSD@users.noreply.github.com>
2025-02-18 17:52:23 -0330

    Added person class

---
+++ Person.java
@@ -0,0 +1,55 @@
+public class Person {
+    private String id;
+    private String name;
+    private int age;
+    private String phoneNumber;
+
+    public Person(String id, String name, int age, String phoneNumber) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.phoneNumber = phoneNumber;
+    }
+    
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+    
+    @Override
+    public String toString() {
+        return "Person{" +
+                "ID='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                '}';
+    }
+}
