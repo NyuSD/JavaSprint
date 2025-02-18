1ccb75273a02688ab141596ef7655ea52b0b501e
NyuSD <161166895+NyuSD@users.noreply.github.com>
2025-02-18 18:20:48 -0330

    Added patient class

--- Patient.java
+++ Patient.java
@@ -1,3 +1,41 @@
-public class Patient {
-    
+import java.util.ArrayList;
+import java.util.List;
+
+public class Patient extends Person {
+    private List<Medication> medications;
+    private List<Prescription> prescriptions;
+
+    public Patient(String id, String name, int age, String phoneNumber) {
+        super(id, name, age, phoneNumber);
+        this.medications = new ArrayList<>();
+        this.prescriptions = new ArrayList<>();
+    }
+
+    public List<Medication> getMedications() {
+        return medications;
+    }
+
+    public void addMedication(Medication medication) {
+        this.medications.add(medication);
+    }
+
+    public List<Prescription> getPrescriptions() {
+        return prescriptions;
+    }
+
+    public void addPrescription(Prescription prescription) {
+        this.prescriptions.add(prescription);
+    }
+
+    @Override
+    public String toString() {
+        return "Patient{" +
+                "ID='" + getId() + '\'' +
+                ", name='" + getName() + '\'' +
+                ", age=" + getAge() +
+                ", phoneNumber='" + getPhoneNumber() + '\'' +
+                ", medications=" + medications +
+                ", prescriptions=" + prescriptions +
+                '}';
+    }
 }
