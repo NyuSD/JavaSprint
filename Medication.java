adce9d0047767b5bafd2cf20f8f88672fe089b9d
NyuSD <161166895+NyuSD@users.noreply.github.com>
2025-03-01 14:18:13 -0330

    Added medication class

--- Medication.java
+++ Medication.java
@@ -1,3 +1,54 @@
+import java.time.LocalDate;
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Medication {
-    
+    private String id;
+    private String name;
+    private String dose;
+    private int quantityInStock;
+    private LocalDate expiryDate;
+
+    public Medication(String id, String name, String dose, int quantityInStock) {
+        this.id = id;
+        this.name = name;
+        this.dose = dose;
+        this.quantityInStock = quantityInStock;
+        long randomDays = ThreadLocalRandom.current().nextLong(-365, 366);
+        this.expiryDate = LocalDate.now().plusDays(randomDays);
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDose() {
+        return dose;
+    }
+
+    public int getQuantityInStock() {
+        return quantityInStock;
+    }
+
+    public void setQuantityInStock(int quantityInStock) {
+        this.quantityInStock = quantityInStock;
+    }
+
+    public LocalDate getExpiryDate() {
+        return expiryDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Medication{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", dose='" + dose + '\'' +
+                ", quantityInStock=" + quantityInStock +
+                ", expiryDate=" + expiryDate +
+                '}';
+    }
 }
